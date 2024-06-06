fdcff4aa70f1b0a635a169ac442609abeae0c621
André Felipe <dede.f99.80@gmail.com>
2024-06-05 21:09:14 -0300

    Implementação dos Cards okay

--- lib/app/data/services/storage/services.dart
+++ lib/app/data/services/storage/services.dart
@@ -2,19 +2,20 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tcase_app/app/core/utils/keys.dart';
 
-class StorageService extends GetxService{
+class StorageService extends GetxService {
   late GetStorage _box;
-  Future<StorageService> init() async{
+  Future<StorageService> init() async {
     _box = GetStorage();
-    await _box.writeIfNull(taskKey, []);
+    await _box.write(taskKey, []);
+    //await _box.writeIfNull(taskKey, []);
     return this;
   }
 
-  T read<T>(String key){
+  T read<T>(String key) {
     return _box.read(key);
   }
 
-  void write(String key, dynamic value) async{
+  void write(String key, dynamic value) async {
     await _box.write(key, value);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tcase_app/app/data/services/storage/services.dart';
+import 'package:tcase_app/app/modules/home/binding.dart';
 import 'package:tcase_app/app/modules/home/view.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() async {
   await GetStorage.init();
@@ -15,9 +17,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'TCase App',
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      initialBinding: HomeBinding(),
+      builder: EasyLoading.init(),
     );
   }
 }
